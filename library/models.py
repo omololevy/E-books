18326078c9408167e11ce5c02f907ab593d0d8fd
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2022-01-10 23:34:58 +0300

    Add profile

--- library/models.py
+++ library/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image',blank=True)
+    bio = models.CharField(max_length=255, blank=True)
+    contacts = models.CharField(max_length=200)
+    join_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
