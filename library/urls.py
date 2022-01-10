2ba98aedd42d9f7eb31d7ddec6946baf0fbde68d
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2022-01-10 23:46:10 +0300

    Add paths

---
+++ library/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('create_profile/', views.create_profile, name='create_profile'),
+    path('profile/', views.profile, name='profile'),
+    path('search/', views.search_results, name='search'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
